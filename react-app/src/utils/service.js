a5ab2d0347369833cfa9fa5fd76fc10a1a108868
hanxiaoluan <1140279968@qq.com>
2020-03-22 21:47:13 +0800

    feat: 🎸 新增loading动画效果
    只是在textStorage里新增loading的动画效果

--- react-app/src/utils/service.js
+++ react-app/src/utils/service.js
@@ -1,14 +1,31 @@
-import axios from "axios";
-import URL from "@/constants/index.js";
-
+import axios from 'axios';
+import URL from '@/constants/index.js';
+import { message } from 'antd';
+let loading = null;
 const SPAIR = URL.baseURL;
+axios.interceptors.request.use(
+  async config => {
+    loading = message.loading({
+      content: '请求中',
+      duration: 0,
+    });
 
+    return config;
+  },
+  error => Promise.reject(error),
+);
+axios.interceptors.response.use(response => {
+  message.destroy();
+  if (response.status === 200 || response.status === 201)
+    return Promise.resolve(response);
+});
 export default class SpairService {
   constructor(namespace) {
     this.namespace = namespace;
   }
   async get(key) {
-    const res = (await axios.get(`${SPAIR}/${this.namespace}/${key}`)) || "";
+    const res = (await axios.get(`${SPAIR}/${this.namespace}/${key}`)) || '';
+    console.log(res);
     return decodeURIComponent(res.data);
   }
   post(key, value) {
