edf71efc0269c0971e81d0edd5338332aefa4a02
hanxiaoluan <1140279968@qq.com>
2020-03-23 17:39:53 +0800

    feat: 🎸 新增登录模块
    只是新增了登录的静态模块

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import React from 'react';
 import {
   HashRouter as Router,
   Switch,
   Route,
   Link,
-  Redirect
-} from "react-router-dom";
+  Redirect,
+} from 'react-router-dom';
 import loadable from './utils/loadable';
-import "./App.css";
+import './App.css';
 import '@/styles/base.scss';
 const Layout = loadable(() => import('./containers/Layout.jsx'));
+const Login = loadable(() => import('@/views/Login/Index'));
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact render={() => <Redirect to="/dashboard" />}></Route>
+        <Route
+          path="/"
+          exact
+          render={() => <Redirect to="/dashboard" />}
+        ></Route>
+        <Route path="/login" component={Login}></Route>
         <Route component={Layout}></Route>
       </Switch>
     </Router>
--- react-app/src/views/Login/Index.jsx
+++ react-app/src/views/Login/Index.jsx
@@ -1,10 +1,76 @@
-import React, { Component } from 'react';
-class Login extends Component {
-  constructor(props) {
-    super(props)
-  }
-  render() {
-    return <div>Login</div>
-  }
-}
-export default Login
+import React, { PureComponent } from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import store from '../../configureStore';
+import axios from 'axios';
+const Login = () => {
+  const [form] = Form.useForm();
+  const handleSubmit = () => {
+    console.log(form.getFieldValue(), form);
+  };
+
+  return (
+    <>
+      <Form
+        form={form}
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+      >
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Username!',
+            },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Password!',
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+            onClick={() => handleSubmit()}
+          >
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
+export default Login;
